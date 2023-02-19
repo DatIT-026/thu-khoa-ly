d83ad887f5732a6c64953007114ce97f850cb389
FireHelper <79629118+DatIT-026@users.noreply.github.com>
2023-02-19 22:50:48 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Test
+# Lưu ý
+Trang web được lập ra với mục đích giải trí, TUYỆT ĐỐI không được sử dụng nó cho mục đích phi pháp, trái quy định pháp luật.
+
+The website is created for entertainment, NEVER use it for illegal purposes, contrary to the law.
+
+### For Help, please contact here: https://www.facebook.com/datit.dev/
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hế lô cậu!',
+    introDesc: `Tớ có thứ này muốn cho cậu xem, nhưng cũng có điều này tớ cũng muốn hỏi cậu, cậu nhớ phải trả lời thật lòng đó nha`,
+    btnIntro: 'Oke nà',
+    title: 'Cậu yêu tớ có phải không? 😙',
+    desc: 'Cậu không trả lời hoặc thoát ra, tức là cậu yêu tớ đó nha :> ',
+    btnYes: 'Yêu ơi là iuuu <33',
+    btnNo: 'Cậu mơ à :333',
+    question:'Nói cho tới lý do vì sao cậu yêu tớ đii ❤',
+    btnReply: 'Gửi cho tớ ❤',
+    reply: 'Tại vì cậu rất đẹp trai! UwU',
+    mess: 'Tớ biết mà 🥰. Cảm ơn cậu 😘😘',
+    messDesc: 'Tớ có quà cho cậu đây',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://firehelper.tk/love.html'
+}
